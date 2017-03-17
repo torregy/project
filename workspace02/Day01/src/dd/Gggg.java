502751a72041047a586f02338d025939d56dc090
torregy <torregy@naver.com>
2017-03-17 15:20:05 +0900

    first commit

---
+++ workspace02/Day01/src/dd/Gggg.java
@@ -0,0 +1,12 @@
+package dd;
+
+public class Gggg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ddd");
+		System.out.println("dd");
+		System.out.println("Dd");
+	}
+
+}
